009b98768dd0336c86a0e2af753ec3bd71343401
Simon <szymon.dzialowski@gmail.com>
2018-12-25 15:07:53 +0000

    catcb

--- example/test.js
+++ example/test.js
@@ -1,6 +1,8 @@
 
 const knex              = require('@stopsopa/knex-abstract');
 
+const log               = require('@stopsopa/knex-abstract/log/logn');
+
 const config            = require('./models/config');
 
 knex.init(config);
@@ -9,9 +11,20 @@ knex.init(config);
 
     const man       = knex().model.common;
 
-    const databases = await man.query(`show databases`);
+    try {
 
-    console.log(JSON.stringify(databases, null, 4));
+        const databases = await man.query(`show databases`);
+
+        console.log(JSON.stringify(databases, null, 4));
+
+        console.log(`count: ` + databases.length);
+
+
+    }
+    catch (e) {
+
+        log.dump(e, 2);
+    }
 
     man.destroy();
 }());
