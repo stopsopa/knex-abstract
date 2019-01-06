961b66dd3b00403a4bb49a177983066e0ebd01f4
Simon <szymon.dzialowski@gmail.com>
2019-01-06 03:24:53 +0000

    since MySQL 5.6.5

--- migrations/src/entity/Users.ts
+++ migrations/src/entity/Users.ts
@@ -72,14 +72,14 @@ export class Users {
     enabled: boolean;
 
     @Column("datetime", {
-        default: () => 'CURRENT_TIMESTAMP',
+        default: () => 'CURRENT_TIMESTAMP', // available for DATETIME since MySQL 5.6.5 https://stackoverflow.com/a/168832/5560682
     })
     created: Date;
 
     @Column("datetime", {
         default: () => null,
         nullable: true,
-        onUpdate: "CURRENT_TIMESTAMP"
+        onUpdate: "CURRENT_TIMESTAMP" // available for DATETIME since MySQL 5.6.5 https://stackoverflow.com/a/168832/5560682
     })
     updated: Date;
 
