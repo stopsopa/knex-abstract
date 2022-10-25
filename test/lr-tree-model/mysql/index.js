4cafbbd6447d9f29ec9c68d656ca73590ce191f2
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2022-10-25 23:09:24 +0100

    refactor: no proxy

--- test/lr-tree-model/mysql/index.js
+++ test/lr-tree-model/mysql/index.js
@@ -4,23 +4,11 @@ const users = require("./users");
 
 const tree = require("./tree");
 
-const managers = {
+/**
+ * http://2ality.com/2014/12/es6-proxies.html
+ */
+module.exports = {
   common,
   users,
   tree,
 };
-
-/**
- * http://2ality.com/2014/12/es6-proxies.html
- */
-module.exports = new Proxy(managers, {
-  get(target, propKey, receiver) {
-    if (typeof target[propKey] !== "undefined") {
-      return target[propKey];
-    }
-
-    const keys = Object.keys(target);
-
-    throw new Error(`No such mysql manager '${propKey}', registered managers are: ` + keys.join(", "));
-  },
-});
